bc70861874fa9eb3c0ee542e6149cbd0cc777667
dodep <dodepmail@mail.ru>
2020-04-01 13:04:20 +0300

    catch all url

--- dist/server.js
+++ dist/server.js
@@ -1,20 +1,9 @@
 const express = require('express')
-const path = require('path')
-const history = require('connect-history-api-fallback')
-// ^ middleware to redirect all URLs to index.html
-
+const port = process.env.PORT || 5000
 const app = express()
-const staticFileMiddleware = express.static(path.join(__dirname))
-
-app.use(staticFileMiddleware)
-app.use(history())
-app.use(staticFileMiddleware)
-// ^ `app.use(staticFileMiddleware)` is included twice as per https://github.com/bripkens/connect-history-api-fallback/blob/master/examples/static-files-and-index-rewrite/README.md#configuring-the-middleware
-
-app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/index.html'))
+app.use(express.static(__dirname))
+app.get(/.*/, function(req,res){
+    res.sendFile(__dirname + "/index.html")
 })
-
-app.listen(5000, function () {
-  console.log( 'Express serving on 5000!' )
-})
+app.listen(port)
+console.log('server started ' + port)
